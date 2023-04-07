cadc4352611ac0a0ec48b5a4073712c64f29f254
fabianrwth <fabian.nolte@rwth-aachen.de>
2023-04-07 22:19:37 +0200

    Edit fixed bugs

--- inference_classifier.py
+++ inference_classifier.py
@@ -4,20 +4,21 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-model_dict = pickle.load(open('./model.p', 'rb'))
-model = model_dict['model']
+model_dict = pickle.load(open("./model.p", "rb"))
+model = model_dict["model"]
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3, max_num_hands=1)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'L'}
+labels_dict = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5"}
+
+
 while True:
-
     data_aux = []
     x_ = []
     y_ = []
@@ -36,7 +37,8 @@ while True:
                 hand_landmarks,  # model output
                 mp_hands.HAND_CONNECTIONS,  # hand connections
                 mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style())
+                mp_drawing_styles.get_default_hand_connections_style(),
+            )
 
         for hand_landmarks in results.multi_hand_landmarks:
             for i in range(len(hand_landmarks.landmark)):
@@ -52,23 +54,27 @@ while True:
                 data_aux.append(x - min(x_))
                 data_aux.append(y - min(y_))
 
+        print(min(x_))
+
         x1 = int(min(x_) * W) - 10
         y1 = int(min(y_) * H) - 10
 
-        x2 = int(max(x_) * W) - 10
-        y2 = int(max(y_) * H) - 10
+        x2 = int(max(x_) * W) + 10
+        y2 = int(max(y_) * H) + 10
 
         prediction = model.predict([np.asarray(data_aux)])
 
         predicted_character = labels_dict[int(prediction[0])]
 
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
-                    cv2.LINE_AA)
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
 
-    cv2.imshow('frame', frame)
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
     cv2.waitKey(1)
-
 
 cap.release()
 cv2.destroyAllWindows()

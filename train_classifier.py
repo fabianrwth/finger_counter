e53fb59b6fcaed405ec3743fc362802bf269f058
fabianrwth <fabian.nolte@rwth-aachen.de>
2023-04-09 14:04:04 +0200

    Add: Changed model path

--- train_classifier.py
+++ train_classifier.py
@@ -24,6 +24,6 @@ score = accuracy_score(y_predict, y_test)
 
 print("{}% of samples were classified correctly !".format(score * 100))
 
-f = open("model.p", "wb")
+f = open("./model/model.p", "wb")
 pickle.dump({"model": model}, f)
 f.close()
